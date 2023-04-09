0d55d018dabfa9ed16e0ed9a116fcb5b005558a5
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2023-04-09 14:42:41 +0400

    add blog

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,43 +1,43 @@
-// import React from 'react'
-// import { Card } from 'antd';
-
-// const { Meta } = Card;
-// function Blog() {
-//     const image = 'https://example.com/react101.png';
-//   const title = 'React 101';
-//   const date = '01/01/2023';
-//   const author = 'John Doe';
-//   const commentCount = 3;
-//   const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo mi a est posuere, eu accumsan ex molestie. Vivamus a purus turpis. Aenean vitae metus at dolor mollis tristique.';
-
-//   return (
-//     <div>
-//         <Card
-//       hoverable
-//       style={{ width: '100%' }}
-//       cover={<img alt={title} src={image} />}
-//     >
-//       <Meta title={title} />
-//       <p>{date} by {author}</p>
-//       <p>{text}</p>
-//       <p>{commentCount} comments</p>
-//       <a href="#">Read more</a>
-//     </Card>
-      
-//     </div>
-//   )
-// }
-
-// export default Blog
 import React from 'react'
+import { Card } from 'antd';
 
+const { Meta } = Card;
 function Blog() {
+    const image = 'https://example.com/react101.png';
+  const title = 'React 101';
+  const date = '01/01/2023';
+  const author = 'John Doe';
+  const commentCount = 3;
+  const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo mi a est posuere, eu accumsan ex molestie. Vivamus a purus turpis. Aenean vitae metus at dolor mollis tristique.';
+
   return (
     <div>
+        <Card
+      hoverable
+      style={{ width: '100%' }}
+      cover={<img alt={title} src={image} />}
+    >
+      <Meta title={title} />
+      <p>{date} by {author}</p>
+      <p>{text}</p>
+      <p>{commentCount} comments</p>
+      <a href="#">Read more</a>
+    </Card>
       
     </div>
   )
 }
 
 export default Blog
+// import React from 'react'
+
+// function Blog() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default Blog
 
