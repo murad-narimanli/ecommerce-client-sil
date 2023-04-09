a4e4abb52573e4dd5587a338dfb14f29a4148a78
KhaligaIbrahimli <khaligaibrahimli@gmail.com>
2023-04-09 17:12:57 +0400

    09-04-2023 17:12 Header and Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,99 @@
-const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    )
+// import React from "react";
+
+// function Footer() {
+//   return (
+//     <div className="footer-container">
+//       <div className="footer-info">
+//         <h2>Bravo</h2>
+//         <p>+994 12 000 0000</p>
+//         <p>+994 50 000 0000</p>
+//         <p>info@bravo.az</p>
+//         <p>www.bravo.az</p>
+//       </div>
+//       <div className="footer-links">
+//         <h3>Mağazalarımız</h3>
+//         <ul>
+//           <li>Ahmedli</li>
+//           <li>20 Yanvar</li>
+//           <li>Koroglu</li>
+//           <li>Ganjlik Mall</li>
+//           <li>Park Azure</li>
+//         </ul>
+//       </div>
+//       <div className="footer-links">
+//         <h3>Haqqımızda</h3>
+//         <ul>
+//           <li>Vakansiyalar</li>
+//           <li>Mağazalar Haritası</li>
+//           <li>Tərəfdaşlarımız</li>
+//           <li>Şərtlər və Qaydalar</li>
+//         </ul>
+//       </div>
+//       <div className="footer-links">
+//         <h3>Mobil Tətbiqətə Keçid</h3>
+//         <button>Applicationa Keçid</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Footer;
+
+import React from "react";
+import Image from "../../assets/image/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+
+function Footer() {
+return (
+<div className="container">
+<div className="row">
+<div className="col-md-3">
+<div className="footer-info">
+<img className="bravologo" src={Image.Bravologo} alt="" style={{ width: "200px" }} />
+<div className="bravotext"style={{ margin: '18px' }}>
+<p>+994 12 000 0000</p>
+<p>+994 50 000 0000</p>
+<p>info@bravo.az</p>
+<p>www.bravo.az</p>
+</div>
+</div>
+</div>
+<div className="col-md-3">
+<div className="footer-links">
+<h3>Mağazalarımız</h3>
+<ul style={{ listStyle: 'none', paddingLeft: '0' }}>
+<li>Ahmedli</li>
+<li>20 Yanvar</li>
+<li>Koroglu</li>
+<li>Ganjlik Mall</li>
+<li>Park Azure</li>
+</ul>
+</div>
+</div>
+<div className="col-md-3">
+<div className="footer-links">
+<h3>Haqqımızda</h3>
+<ul style={{ listStyle: 'none', paddingLeft: '0' }}>
+<li>Vakansiyalar</li>
+<li>Mağazalar Haritası</li>
+<li>Tərəfdaşlarımız</li>
+<li>Şərtlər və Qaydalar</li>
+</ul>
+</div>
+</div>
+<div className="col-md-3">
+<div className="footer-links">
+<h3>Mobil Tətbiqətə Keçid</h3>
+<button className="application" onClick={() => window.open("https://apps.apple.com/us/app/bravo-supermarkets/id1427953617")}>
+   
+   <p className="download"> <FontAwesomeIcon icon={faApple} style={{ fontSize: '25px' }} /> Download on the</p><p> App Store</p>
+</button>
+</div>
+</div>
+</div>
+</div>
+);
 }
 
-export default Footer
+export default Footer;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,33 +1,103 @@
+// import React from "react";
+// import "../../assets/scss/Header.scss"
+// import Menu from "../elements/Menu";
+// import Blog from "../pages/Blog";
+// import Image from "../../assets/image/image";
+// import Searchnav from "../elements/Searchbar";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faShoppingCart, faHeart } from '@fortawesome/free-solid-svg-icons';
+
+
+
+// const Header = () => {
+//   return(
+//     <div>
+//     <nav className="navbar navbar-expand-lg navbar-light ">
+//         <div className="container">
+//        <div style={{ width: "100%" }}>
+//        <img className="bravologo" src={Image.Bravologo} alt="" style={{ width: "200px" }} />
+//        </div>
+//           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//             <li className="nav-item">
+//       <Searchnav />
+//               </li>
+//               <li className="nav-item">
+//   <div className="login-buttons">
+//     <div className="login-button enter">DAXILOL</div>
+//     <div className="login-button">QEYDIYYAT</div>
+//   </div>
+// </li>
+// <div className="login-button">
+//     <FontAwesomeIcon icon={faShoppingCart} />
+//   </div>
+//   <div className="login-button">
+//     <FontAwesomeIcon icon={faHeart} />
+//   </div>
+
+             
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+// <Menu/>
+//     <Blog/>
+//   </div>
+//   )
+// };
+
+// export default Header;
+
 import React from "react";
 import "../../assets/scss/Header.scss"
 import Menu from "../elements/Menu";
 import Blog from "../pages/Blog";
 import Image from "../../assets/image/image";
 import Searchnav from "../elements/Searchbar";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return(
     <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light ">
         <div className="container">
-       <div style={{ width: "100%" }}>
-       <img className="bravologo" src={Image.Bravologo} alt="" style={{ width: "200px" }} />
-       </div>
+          <div style={{ width: "100%" }}>
+            <img className="bravologo" src={Image.Bravologo} alt="" style={{ width: "200px" }} />
+          </div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-      <Searchnav />
+                <Searchnav />
               </li>
-             
+              <li className="nav-item">
+                <div className="login-buttons">
+                  <div className="login-button enter">DAXILOL</div>
+                  <div className="login-button">QEYDIYYAT</div>
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="login-button cart">
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                </div>
+              </li>
+              <li className="nav-item">
+                <div className="login-button heart">
+                  <FontAwesomeIcon icon={faHeart} />
+                </div>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
-<Menu/>
-    <Blog/>
-  </div>
-  )
+      <Menu />
+      <Blog />
+    </div>
+  );
 };
 
 export default Header;
+
+
+
+
