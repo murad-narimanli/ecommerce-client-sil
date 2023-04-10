265667fbd5cf4608c14cb23b731caa5adb298b7e
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2023-04-10 17:18:43 +0400

    try for problem

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,57 +1,15 @@
-// import React from "react";
-
-// function Footer() {
-//   return (
-//     <div className="footer-container">
-//       <div className="footer-info">
-//         <h2>Bravo</h2>
-//         <p>+994 12 000 0000</p>
-//         <p>+994 50 000 0000</p>
-//         <p>info@bravo.az</p>
-//         <p>www.bravo.az</p>
-//       </div>
-//       <div className="footer-links">
-//         <h3>Mağazalarımız</h3>
-//         <ul>
-//           <li>Ahmedli</li>
-//           <li>20 Yanvar</li>
-//           <li>Koroglu</li>
-//           <li>Ganjlik Mall</li>
-//           <li>Park Azure</li>
-//         </ul>
-//       </div>
-//       <div className="footer-links">
-//         <h3>Haqqımızda</h3>
-//         <ul>
-//           <li>Vakansiyalar</li>
-//           <li>Mağazalar Haritası</li>
-//           <li>Tərəfdaşlarımız</li>
-//           <li>Şərtlər və Qaydalar</li>
-//         </ul>
-//       </div>
-//       <div className="footer-links">
-//         <h3>Mobil Tətbiqətə Keçid</h3>
-//         <button>Applicationa Keçid</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Footer;
-
 import React from "react";
-import Image from "../../assets/image/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faApple } from "@fortawesome/free-brands-svg-icons";
+
 
 function Footer() {
 return (
+<div className="footer">
 <div className="container">
 <div className="row">
 <div className="col-md-3">
 <div className="footer-info">
-<img className="bravologo" src={Image.Bravologo} alt="" style={{ width: "200px" }} />
-<div className="bravotext"style={{ margin: '18px' }}>
+<h3 className="f-main">Əlaqə</h3>
+<div className="bravotext f-piece">
 <p>+994 12 000 0000</p>
 <p>+994 50 000 0000</p>
 <p>info@bravo.az</p>
@@ -61,22 +19,22 @@ return (
 </div>
 <div className="col-md-3">
 <div className="footer-links">
-<h3>Mağazalarımız</h3>
-<ul style={{ listStyle: 'none', paddingLeft: '0' }}>
-<li>Ahmedli</li>
+<h3 className="f-main">Mağazalarımız</h3>
+<ul className="f-piece"  style={{ listStyle: 'none', paddingLeft: '0' }}>
+<li >Əhmədli</li>
 <li>20 Yanvar</li>
-<li>Koroglu</li>
-<li>Ganjlik Mall</li>
+<li>Koroğlu</li>
+<li>Gənclik Mall</li>
 <li>Park Azure</li>
 </ul>
 </div>
 </div>
 <div className="col-md-3">
 <div className="footer-links">
-<h3>Haqqımızda</h3>
-<ul style={{ listStyle: 'none', paddingLeft: '0' }}>
+<h3 className="f-main">Haqqımızda</h3>
+<ul className="f-piece" style={{ listStyle: 'none', paddingLeft: '0' }}>
 <li>Vakansiyalar</li>
-<li>Mağazalar Haritası</li>
+<li>Mağazalar Xəritəsi</li>
 <li>Tərəfdaşlarımız</li>
 <li>Şərtlər və Qaydalar</li>
 </ul>
@@ -84,11 +42,21 @@ return (
 </div>
 <div className="col-md-3">
 <div className="footer-links">
-<h3>Mobil Tətbiqətə Keçid</h3>
-<button className="application" onClick={() => window.open("https://apps.apple.com/us/app/bravo-supermarkets/id1427953617")}>
-   
-   <p className="download"> <FontAwesomeIcon icon={faApple} style={{ fontSize: '25px' }} /> Download on the</p><p> App Store</p>
-</button>
+<h3 className="f-main">Mobil Tətbiqə Keçid</h3>
+
+<div className="applicationbravo d-flex">
+<div className="application" onClick={() => window.open("https://apps.apple.com/us/app/bravo-supermarkets/id1427953617")}>
+<span className="iconappstore"></span>
+   <span className="download">Download on  </span>
+   <span className="app">App Store</span>
+</div>
+<div className="application" onClick={() => window.open("https://play.google.com/store/apps/details?id=com.nbcu.tve.bravotv.androidtv&hl=ru&gl=US")}>
+<span className="icongoogleplay"></span>
+   <span className="download">Get in on  </span>
+   <span className="app">Google Play</span>
+</div>
+</div>
+</div>
 </div>
 </div>
 </div>
