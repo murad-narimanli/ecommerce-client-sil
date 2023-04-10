9e58b7bab47227c5e212187853c1b4712c78523b
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2023-04-10 18:50:39 +0400

    06:50 04-10-2023 edit menu

--- src/components/elements/Menu.js
+++ src/components/elements/Menu.js
@@ -1,23 +1,37 @@
 import React from "react";
-import { Navbar,  Nav } from "react-bootstrap";
-import "../../assets/scss/Header.scss"
-import {useLocation, NavLink} from "react-router-dom"
+import { Navbar, Nav } from "react-bootstrap";
+import "../../assets/scss/Header.scss";
+import { useLocation, NavLink } from "react-router-dom";
 function Menu() {
-// let location =useLoaction()
-// let {pathname}=location
+  // let location =useLoaction()
+  // let {pathname}=location
 
   return (
     <div className="container">
       <Navbar expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse  >
+        <Navbar.Collapse>
           <Nav className="me-2">
-           <NavLink  to='./'> <Nav.Link href="#link">Ana Səhifə</Nav.Link></NavLink>
-           <NavLink to='./aboutus'> <Nav.Link href="#link" >Haqqımızda</Nav.Link></NavLink>
-           <NavLink to='./blog'> <Nav.Link href="#link" >Bloq</Nav.Link>     </NavLink>      
-           <NavLink to='./shop'> <Nav.Link href="#link" >Məhsullar</Nav.Link>     </NavLink>  
-           <NavLink to='./contactus'> <Nav.Link href="#link" >Əlaqə</Nav.Link>     </NavLink>      
-        
+            <NavLink to="./" className="text-decoration-none">
+              {" "}
+              <Nav.Link href="#link">Ana Səhifə</Nav.Link>
+            </NavLink>
+            <NavLink to="./aboutus" className="text-decoration-none">
+              {" "}
+              <Nav.Link href="#link">Haqqımızda</Nav.Link>
+            </NavLink>
+            <NavLink to="./blog" className="text-decoration-none">
+              {" "}
+              <Nav.Link href="#link">Bloq</Nav.Link>{" "}
+            </NavLink>
+            <NavLink to="./shop" className="text-decoration-none">
+              {" "}
+              <Nav.Link href="#link">Məhsullar</Nav.Link>{" "}
+            </NavLink>
+            <NavLink to="./contactus" className="text-decoration-none">
+              {" "}
+              <Nav.Link href="#link">Əlaqə</Nav.Link>{" "}
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
