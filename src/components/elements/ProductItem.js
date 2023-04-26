dee4f29f711bebb374ce0d0737e18d9d75d7221f
zumrudbaxshaliyeva <zumrudbaxsaliyeva@gmail.com>
2023-04-26 18:22:33 +0400

    06-22 26-04-2023 product CRUD

--- src/components/elements/ProductItem.js
+++ src/components/elements/ProductItem.js
@@ -2,7 +2,8 @@ import React from "react";
 import swal from "sweetalert";
 import { products } from "../../api/db";
 import {
-  HeartFilled,HeartOutlined,
+  HeartFilled,
+  HeartOutlined,
   ShoppingCartOutlined,
   ShareAltOutlined,
 } from "@ant-design/icons";
@@ -11,9 +12,11 @@ import { Card } from "antd";
 import "../../assets/scss/ProductItem.scss";
 import Image from "../../assets/image/image";
 import { useState } from "react";
-import { Switch } from 'antd';
+import { Switch } from "antd";
+import { connect } from "react-redux";
+import { addProduct, editProduct, deleteProduct } from "../../redux/actions";
 const { Meta } = Card;
-function ProductItem() {
+function ProductItem({  addProduct, editProduct, deleteProduct }) {
   let [basket, setBasket] = useState(
     JSON.parse(localStorage.getItem("basket")) || []
   );
@@ -61,81 +64,62 @@ function ProductItem() {
     });
   };
   return (
-  
     <div className="row g-3 ">
       {products.map((product) => (
-   
-          <div className="card col-lg-3 me-3" style={{width: "400px"}} key={product.id}>
-      <img className="card-img-top" src={Image.Product1} alt="Card image"/>
-      <div className="card-body">
-        <h4 className="card-title">{product.name}</h4>
-        <h5 className="card-text">${product.price}</h5>
-        <div className="text-center counter pb-3 pt-3">
-          
-        <div onClick={()=>handleLike(product)} data-id={product.id} className="me-5 ">
-          <span onClick={() => addWishlist(product)}>
-      {isLiked ? (
-        <HeartFilled style={{ color: 'red', fontSize: '24px' }} />
-      ) : (
-        <HeartFilled style={{ color: 'green', fontSize: '24px' }} />
-      )}
-      </span>
-        </div>
-        <div   onClick={() => {
-                  
+        <div
+          className="card col-lg-3 me-3"
+          style={{ width: "400px" }}
+          key={product.id}
+        >
+          <img className="card-img-top" src={Image.Product1} alt="Card image" />
+          <div className="card-body">
+            <h4 className="card-title">{product.name}</h4>
+            <h5 className="card-text">${product.price}</h5>
+            <div className="text-center counter pb-3 pt-3">
+              <div
+                onClick={() => handleLike(product)}
+                data-id={product.id}
+                className="me-5 "
+              >
+                <span onClick={() => addWishlist(product)}>
+                  {isLiked ? (
+                    <HeartFilled style={{ color: "red", fontSize: "24px" }} />
+                  ) : (
+                    <HeartFilled style={{ color: "green", fontSize: "24px" }} />
+                  )}
+                </span>
+              </div>
+              <div
+                onClick={() => {
                   Alert();
-                }}>
-      <ShoppingCartOutlined  className="ms-3"
-        style={{ color: 'green', fontSize: '24px' }}
-              // className="basket-icon"
-              onClick={() => addToCart(product)}
-              data-id={product.id }
-            />
-    
-  </div>
-        
-      
-            
+                }}
+              >
+                <ShoppingCartOutlined
+                  className="ms-3"
+                  style={{ color: "green", fontSize: "24px" }}
+                  // className="basket-icon"
+                  onClick={() => addToCart(product)}
+                  data-id={product.id}
+                />
+              </div>
+            </div>
           </div>
-      </div>
-    </div>
-
-  
-      
-        
+        </div>
       ))}
     </div>
-    
   );
 }
-export default ProductItem;
-// <Card
 
-//   style={{
-//     width: 300,
-//   }}
-//   cover={<img alt="example" src={Image.Product1} />}
-//   actions={[
-//     <HeartFilled className="heart-icon" />,
-//     <ShoppingCartOutlined className="basket-icon" />,
-//     <ShareAltOutlined className=" share-icon" />,
-//   ]}
-// >
-//   <Meta title="KENDIMIZ BAL 900 GR AKASIYA..." />
-//   <div className="text-center counter pb-3 pt-3">
-//     <button className="product_btn me-3" onClick={inc}>+</button>
-//     {count}
-//     <button className="product_btn ms-3" onClick={dec}>-</button>
-//   </div>
-//   <Meta
-//     description={
-//       <div>
-//         <h1 className="product-price">₼ 5,19 ədəd</h1>
-//         <span style={{ color: "#BB0000", fontFamily: "Arial" }}>
-//           {<FallOutlined className="discount-icon me-3" />}Endirim
-//         </span>
+// const mapStateToProps = ({ products }) => ({
+//   products,
+// });
 
-//       </div>
-//     }
-//   />
-// </Card>
+const mapStateToProps = (state) => ({
+  products: state.products,
+});
+
+export default connect(mapStateToProps, {
+  addProduct,
+  editProduct,
+  deleteProduct,
+})(ProductItem);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "../src/App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+root.render( <Provider store={store} >  <App />  </Provider> );
 
