d7d52bc33464e1ab83cc8146deae369e3af92c99
KhaligaIbrahimli <khaligaibrahimli@gmail.com>
2023-04-30 19:10:00 +0400

    blog blog detail

--- src/components/elements/Discount.js
+++ src/components/elements/Discount.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import client from "../../api/api";
+
+
 
 function Discount() {
+  const [data, setData]=useState ({})
+
+  useEffect(()=>{
+    getdata()
+ 
+   }, []);
+   const getdata = async()=>{
+     await client.get('discountnews').then((res)=>{
+       if(res.data.length){
+         setData(res.data[0])
+       }
+     })
+   }
+ 
   return (
     <div className="p-3 discount mt-3">
       <span className="dicount_text">
-        Bravo bonus kartı əldə et cashback qazan.
+      <div dangerouslySetInnerHTML={{ __html: data.discountnews}} />
+
       </span>
     </div>
   );
